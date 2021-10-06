90fe9646bf6985fac8de47fff226f84ba3fdcc66
Curo-Dev <admin@curo.xyz>
2021-10-06 14:24:27 +0900

    feat: orm config 내부에 바로 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nestjs_curd',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
