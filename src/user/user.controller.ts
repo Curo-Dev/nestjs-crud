2cc96612f20f231b7a52a9a551d10d0ae07cd68f
Curo-Dev <admin@curo.xyz>
2021-10-06 14:25:39 +0900

    feat: 테스트용 설계

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -6,7 +7,10 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getHello(): string {
-    return this.userService.getHello();
+  getUser() {
+    const user = new UserDto();
+    user.username = 'curo-dev';
+    user.password = 'asdasda';
+    return this.userService.createOne(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private userRepository: UserRepository) {}
+
+  createOne(user: UserDto) {
+    return this.userRepository.createOne(user);
   }
 }
